9aa348ebf34e1487d37fda67487f69fc5b019838
김민제 <mu8786@gmail.com>
2022-12-11 12:20:04 +0900

    에러 던지는 부분 삭제

--- hello-world/app.js
+++ hello-world/app.js
@@ -15,21 +15,21 @@ let response;
  *
  */
 exports.lambdaHandler = async (event, context) => {
-  // try {
-  //     // const ret = await axios(url);
-  //     response = {
-  //         'statusCode': 200,
-  //         'body': JSON.stringify({
-  //             message: `I'm using canary deployments`,
-  //             // location: ret.data.trim()
-  //         })
-  //     }
-  // } catch (err) {
-  //     console.log(err);
-  //     return err;
-  // }
+  try {
+    // const ret = await axios(url);
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: `I'm using canary deployments`,
+        // location: ret.data.trim()
+      }),
+    };
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
 
-  throw new Error('This will cause a deployment rollback');
+  //   throw new Error('This will cause a deployment rollback');
 
   return response;
 };
